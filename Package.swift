859095c9aebe3f5dad2d460689cbbc991018c524
Kishin Manglani <kishin.manglani@gmail.com>
2021-11-23 21:54:15 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,23 @@
 import PackageDescription
 
 let package = Package(
-    name: "RangeSeekSlider"
+    name: "RangeSeekSlider",
+    platforms: [
+        .iOS(.v11),
+    ],
+    products: [
+        .library(
+            name: "RangeSeekSlider",
+            targets: ["RangeSeekSlider"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "RangeSeekSlider",
+            path: "Sources",
+            exclude: [
+                "Info.plist",
+            ]
+        ),
+    ]
 )
